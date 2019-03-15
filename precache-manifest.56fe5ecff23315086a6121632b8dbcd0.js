626bfaa02705d3e972ba01d36d5a6dedb5cf7264
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-03-15 17:08:15 -0400

    Updates

---
+++ precache-manifest.56fe5ecff23315086a6121632b8dbcd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea8e4b5407155f14fb0b222b39824658",
+    "url": "/siteguide/static/media/UniqloProRegular.ea8e4b54.ttf"
+  },
+  {
+    "revision": "538106203ef20ff72d001cec96409479",
+    "url": "/siteguide/static/media/UniqloProBold.53810620.ttf"
+  },
+  {
+    "revision": "29163b2252bd38a9e89a",
+    "url": "/siteguide/static/js/runtime~main.0f334ef9.js"
+  },
+  {
+    "revision": "11dfa310fd6b90bde144",
+    "url": "/siteguide/static/js/main.90979278.chunk.js"
+  },
+  {
+    "revision": "aa9945b35ccd832dcc0c",
+    "url": "/siteguide/static/js/2.4bdb9adc.chunk.js"
+  },
+  {
+    "revision": "11dfa310fd6b90bde144",
+    "url": "/siteguide/static/css/main.87f686ec.chunk.css"
+  },
+  {
+    "revision": "aa9945b35ccd832dcc0c",
+    "url": "/siteguide/static/css/2.5d33b571.chunk.css"
+  },
+  {
+    "revision": "707f2d8edbf6fc8539cc8dcaf8b2a5e6",
+    "url": "/siteguide/index.html"
+  }
+];
